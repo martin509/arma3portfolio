8139df0507b0d2b270bd091da4f7458061fcdd5c
Martin <martin.rivard@teksavvy.com>
2025-01-01 17:51:44 -0500

    Livonian Lighting for Dingor

---
+++ Livonian Lighting - Dingor/mod/mod.cpp
@@ -0,0 +1,7 @@
+name="Livonian Lighting: Dingor";
+tooltipOwned="Making Dingor less blue";
+description = "Changes Dingor's lighting to that of Livonia's.";
+overview = "Livonian Lighting: Dingor";
+logoSmall = "logo.paa";
+logo = "logo.paa";
+logoOver = "logo.paa";
